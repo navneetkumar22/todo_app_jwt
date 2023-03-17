47184b8a831742d5583a3ad52bae8c705e12c0e7
Navneet Kumar <navneetsinghpanwar90@gmail.com>
2023-03-17 19:09:00 +0530

    added all crud routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { home, createTodo } = require('../controllers/todoControllers');
+const { home, createTodo, getAllTodos, editTodo, deleteTodo, getAllTasks, addTask, editTask, deleteTask } = require('../controllers/todoControllers');
 const { register, login, dashboard, logout } = require('../controllers/userControllers');
 const auth = require('../middlewares/auth');
 
@@ -7,7 +7,17 @@ const router = express.Router();
 
 //Todo Routes
 router.get("/", home);
-router.post("/api/v1/todo/create", auth, createTodo);
+router.post("/api/todo/create", auth, createTodo);
+router.get("/getAllTodos", getAllTodos);
+router.put("/editTodo/:id", editTodo);
+router.delete("/deleteTodo/:id", deleteTodo);
+
+//Task Routes
+router.get("/getAllTasks/:id", getAllTasks);
+router.post("/addTask/:id", addTask);
+router.put("/editTask/:todoId/:taskId", editTask);
+router.delete("/deleteTask/:todoId/:taskId", deleteTask);
+
 
 //User Routes
 router.post("/register", register);
