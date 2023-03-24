0c49e43d582a9fd241230ff52446e2cfedda21b1
Navneet Kumar <navneetsinghpanwar90@gmail.com>
2023-03-24 10:30:06 +0530

    some changes

--- frontend/src/components/SignUp.jsx
+++ frontend/src/components/SignUp.jsx
@@ -1,20 +1,31 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import axios from "axios";
 
 function Signup() {
     const navigate = useNavigate();
-    const [user, setUser] = useState({
-        name: "",
-        email: "",
-        password: ""
-    })
+    const [userName, setUserName] = useState('');
+    const [userEmail, setUserEmail] = useState('');
+    const [userPassword, setUserPassword] = useState('');
+
+    const submitData = async () => {
+        const data = { name: userName, email: userEmail, password: userPassword }
+        try {
+            await axios.post("http://localhost:4000/register", data);
+            
+        } catch (error) {
+            console.log(error)
+        }
+    }
 
     // sign up
     const signupUser = async (e) => {
         e.preventDefault()
+        submitData();
 
-
-
+        setUserName('');
+        setUserEmail('');
+        setUserPassword('');
     }
 
 
@@ -22,11 +33,11 @@ function Signup() {
         <div className='login-div'>
             <div className="form-div">
                 <h1>Sign Up</h1>
-                <form className='login-form' action="">
-                    <input type="text" id='name' name='name' placeholder='Name' onChange={(e) => { setUser({ ...user, name: e.target.value }) }} />
-                    <input type="email" id='email' name='email' placeholder='Email' onChange={(e) => { setUser({ ...user, email: e.target.value }) }} />
-                    <input type="password" id='password' name='password' placeholder='Password' onChange={(e) => { setUser({ ...user, password: e.target.value }) }} />
-                    <button type='submit' onClick={signupUser}>Submit</button>
+                <form className='login-form' onSubmit={signupUser}>
+                    <input type="text" id='name' name='name' placeholder='Name' value={userName} onChange={(e) => { setUserName(e.target.value) }} />
+                    <input type="email" id='email' name='email' placeholder='Email' value={userEmail} onChange={(e) => { setUserEmail(e.target.value) }} />
+                    <input type="password" id='password' name='password' placeholder='Password' value={userPassword} onChange={(e) => { setUserPassword(e.target.value) }} />
+                    <button type='submit'>Submit</button>
                 </form>
                 <h4>Already have an account?</h4>
                 <div className="social">
