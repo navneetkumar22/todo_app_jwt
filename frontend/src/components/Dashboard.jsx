c5ef9552085d0dc9dcc2343f08f17436550bfc2d
Navneet Kumar <navneetsinghpanwar90@gmail.com>
2023-04-04 23:20:54 +0530

    dashboard progress

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -1,88 +1,126 @@
 import React, { useEffect, useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 
 
 function Dashboard() {
-    const navigate = useNavigate();
-    const [userData, setUserData] = useState();
+    const [userData, setUserData] = useState('');
+    const [todos, setTodos] = useState([]);
+    const [tasks, setTasks] = useState('');
 
-    useEffect(() => { }, [])
+    //fetch the current user
+    const fetchUser = async () => {
+        const resp = await axios.get("http://localhost:4000/dashboard", {
+            headers: { "Authorization": `Bearer ${localStorage.getItem("token")}` }
+        })
+        if (resp.data.user.length !== 0) { setUserData(resp.data.user); }
+    };
 
-    const handleLogout = async () => {
-        //
+    // useEffect(() => {
+    //     fetchUser();
+    // }, []);
+
+    // fetch all todos
+    const fetchTodos = async () => {
+        const getTodos = await axios.get("http://localhost:4000/getAllTodos", {
+            headers: { "Authorization": `Bearer ${localStorage.getItem("token")}` }
+        })
+        if (getTodos.data.todos.length > 0) {
+            const finalTodos = getTodos.data.todos.filter(element => element.user == userData._id);
+
+            setTodos(getTodos.data.todos);
+        }
     }
+
+
+    //function to toggle the tasks contents between showing and hiding
+    // function showFaq(a) {
+    //     let contents = document.querySelectorAll(".tasks-div");
+
+    //     if (contents[a].style.display !== 'block') {
+    //         contents[a].style.display = 'block';
+    //     } else if (contents[a].style.display = 'block') {
+    //         contents[a].style.display = 'none';
+    //     }
+    // }
+
+    const showFaq = () => {
+        let content = document.querySelector(".tasks-div");
+
+        if (content.style.display !== 'flex') {
+            content.style.display = 'flex';
+        } else if (content.style.display = 'none') {
+            content.style.display = 'flex';
+        }
+    }
+
+    // let buttons = document.querySelectorAll('.todo-div');
+    // buttons.forEach((e, i) => {
+    //     e.addEventListener('click', () => { showFaq(i) })
+    // });
+
+
+
+
+
+    useEffect(() => {
+        fetchTodos();
+        fetchUser()
+    }, []);
+
+    console.log(todos);
 
     return (
         <>
-            {userData ? (
-                <>
-                    <section className='navigation'>
-                        <div className="navbar">
-                            <div className="welcome">
-                                <h1>Welcome <span className='name'>{userData.name}</span></h1>
-                            </div>
-                            <div className='dashboard-btn'>
+            <div className="todo-app">
+                <h1>Todo Application</h1>
+            </div>
+            <div className="topbar">
+                <h2>Welcome {userData.name}</h2>
+                <div className="nav-buttons">
+                    <button className="create">Create new todo</button>
+                    <input type="search" placeholder="search" />
+                    <button className="logout">Logout</button>
+                </div>
+            </div>
 
-                                <input type="search" name="search" id="search" placeholder='Search' />
-                                <button className='logout' onClick={handleLogout}>Logout</button>
-                            </div>
-                        </div>
-                    </section>
+            <section className="hero">
+                <div className="header">
+                    <div>
+                        <h2>All Todos</h2>
+                    </div>
+                    <div className="sorting">
+                        <h3>Sort By</h3>
+                        <button className="create">Created Date</button>
+                        <button className="create">Modified Date</button>
+                    </div>
+                </div>
+            </section>
 
+            <section className="todos">
+                {todos && todos.map((todo) => (
 
-                    {/* Todo and task Section */}
-
-                    <section className='hero'>
-                        <div className='todo-head'>
-                            <div>
-                                <button className='create-btn'>Create new todo</button>
-                            </div>
-                            <div>
-                                <h2>All Todos</h2>
-                            </div>
-                            <div className="sorting">
-                                <p>Sort By</p>
-                                <select name="sort" id="sort">
-                                    <option value="Created Date" selected>Created Date</option>
-                                    <option value="Modified Date">Modified Date</option>
-                                    <option value="Alphabetic">Alphabetic</option>
-                                </select>
-                            </div>
-                        </div>
-                    </section>
-
-                    <section className='todos'>
-                        <div className="container">
-                            <div className="todo">
-                                <div>Expand to see tasks</div>
-                                <div>todo title</div>
-                                <div>
-                                    <button className='todo-edit'>Edit</button>
-                                    <button className='todo-dlt'>Delete</button>
-                                </div>
-                            </div>
-                            <div className="tasks">
-                                <div className="task">
-                                    <div className="tasktitle"><p>TaskTitle</p></div>
-                                    <div className="task-btn">
-                                        <button>Edit Task</button>
-                                        <button>Delete Task</button>
-                                    </div>
-                                </div>
-
-
-
+                    <div className="todo-div" key={todo._id}>
+                        <div className="todo-name">
+                            <h3>{todo.title}</h3>
+                            <div className="tasks-div">
+                                <p>first task</p>
+                                <p>first task</p>
+                                <p>first task</p>
+                                <p>first task</p>
+                                <p>first task</p>
                             </div>
                         </div>
-                    </section>
-                </>
-            ) : (
-                <p>Please login to add Todo
-                    <Link to="/login">Login
-                    </Link>
-                </p>
-            )}
+                        <div className="action">
+                            <button className="edit-btn">Edit todo</button>
+                            <button className="delete-btn">Delete todo</button>
+                        </div>
+                    </div>
+                ))}
+            </section >
+
+
+
         </>
     )
 }
